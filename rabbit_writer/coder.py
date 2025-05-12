fc832cac7cd22956788f1ee4f1f884a17c5bd725
JFH000 <jf.hernandezp1@uniandes.edu.co>
2025-05-11 21:13:27 -0500

    ms rabbit writer

---
+++ rabbit_writer/coder.py
@@ -0,0 +1,36 @@
+import json
+import base64
+import hmac
+import hashlib
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+def load_keys(file_path="keys.json"):
+    with open(file_path, "r") as f:
+        keys_data = json.load(f)
+
+    keys = {
+        "KEY": base64.b64decode(keys_data["KEY"]),
+        "HMAC_KEY": base64.b64decode(keys_data["HMAC_KEY"]),
+        "IV": base64.b64decode(keys_data["IV"])
+    }
+    return keys
+
+def encrypt_json(data, keys):
+    load_keys()
+    # Convertir diccionario a string JSON
+    json_str = json.dumps(data)
+
+    # Cifrar con AES-CBC
+    cipher = AES.new(keys["KEY"], AES.MODE_CBC, keys["IV"])
+    encrypted_bytes = cipher.encrypt(pad(json_str.encode(), AES.block_size))
+    encrypted_base64 = base64.b64encode(encrypted_bytes).decode()
+
+    # Generar HMAC SHA-256 para verificar integridad
+    hmac_signature = hmac.new(keys["HMAC_KEY"], encrypted_bytes, hashlib.sha256).hexdigest()
+
+    # Retornar como JSON string
+    return json.dumps({
+        "data": encrypted_base64,
+        "hmac": hmac_signature
+    })
---
+++ rabbit_writer/writer.py
@@ -0,0 +1,34 @@
+import pika
+from .coder import encrypt_json
+
+def connect_rabbitmq(host='10.128.0.20', user='isis2503', password='1234'):
+    """
+    Establece conexión con RabbitMQ y retorna un canal.
+    """
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=host,
+            credentials=pika.PlainCredentials(user, password)
+        )
+    )
+    channel = connection.channel()
+    return channel
+
+def publish_message(queue_name, message, channel=connect_rabbitmq()):
+    """
+    Declara la cola (la crea si no existe) y envía el mensaje cifrado.
+    """
+    # Crear la cola si no existe
+    channel.queue_declare(queue=queue_name, durable=True, exclusive=False, auto_delete=False)
+
+    # Cifrar el mensaje
+    encrypted_message = encrypt_json(message)
+
+    # Publicar en la cola
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue_name,
+        body=encrypted_message,
+        properties=pika.BasicProperties(delivery_mode=2)  # Persistente
+    )
+    print(f"Mensaje publicado en la cola '{queue_name}'")
